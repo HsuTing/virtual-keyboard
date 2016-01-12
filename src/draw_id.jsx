5629c88257db42ecea60234abe514ba5cc6668e1
HsuTing <hsuting0106@gmail.com>
2016-01-13 05:26:12 +0800

    add letters

--- src/draw_id.jsx
+++ src/draw_id.jsx
@@ -1,12 +1,31 @@
 'use strict';
 
+import $ from 'jquery';
+
 import {
   Context as Context
 } from './constant';
 
+let list = {
+  '1007': 'A',
+  '827': 'B',
+  '787': 'C',
+  '140': 'J',
+  '750': 'K',
+  '3': 'L',
+  '955': 'M',
+  '425': 'Q',
+  '179': 'R',
+  '48': 'T',
+  '393': 'V',
+  '204': 'X',
+  '666': 'Y'
+};
+
 export default (markers) => {
   Context.strokeStyle = "blue";
   Context.lineWidth = 1;
+  let check = {}; 
  
   for (let i = 0; i < markers.length; i++){
     let corners = markers[i].corners;
@@ -20,5 +39,12 @@ export default (markers) => {
     }
 
     Context.strokeText(markers[i].id, x, y)
+    check[markers[i].id] = true;
+  }
+
+  for(let num in list) {
+    if(!check[num]) {
+      $('#info').html(list[num]);
+    }
   }
 };
