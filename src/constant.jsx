da31b6e4dc6e4d5f2fe399c539890eb71c6a563f
HsuTing <hsuting0106@gmail.com>
2016-01-15 04:03:17 +0800

    only a,s,d,w

--- src/constant.jsx
+++ src/constant.jsx
@@ -4,10 +4,10 @@ let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let list = {
   '0': { 'label': 'A', 'parent': ['Q'] },
-  '1': { 'label': 'B', 'parent': ['G', 'T'] },
-  '2': { 'label': 'C', 'parent': ['D', 'E'] },
+/*  '1': { 'label': 'B', 'parent': ['G', 'T'] },
+  '2': { 'label': 'C', 'parent': ['D', 'E'] },*/
   '3': { 'label': 'D', 'parent': ['E'] },
-  '4': { 'label': 'E', 'parent': [] },
+/*  '4': { 'label': 'E', 'parent': [] },
   '5': { 'label': 'F', 'parent': ['R'] },
   '6': { 'label': 'G', 'parent': ['T'] },
   '7': { 'label': 'H', 'parent': ['Y'] },
@@ -20,15 +20,16 @@ let list = {
   '14': { 'label': 'O', 'parent': [] },
   '15': { 'label': 'P', 'parent': [] },
   '16': { 'label': 'Q', 'parent': [] },
-  '17': { 'label': 'R', 'parent': [] },
+  '17': { 'label': 'R', 'parent': [] },*/
   '18': { 'label': 'S', 'parent': ['W'] },
-  '19': { 'label': 'T', 'parent': [] },
+/*  '19': { 'label': 'T', 'parent': [] },
   '20': { 'label': 'U', 'parent': [] },
-  '21': { 'label': 'V', 'parent': ['F', 'R'] },
-  '22': { 'label': 'W', 'parent': [] },
-  '23': { 'label': 'X', 'parent': ['S', 'W'] },
+  '21': { 'label': 'V', 'parent': ['F', 'R'] },*/
+  '22': { 'label': 'W', 'parent': [] }
+/*  '23': { 'label': 'X', 'parent': ['S', 'W'] },
   '24': { 'label': 'Y', 'parent': [] },
   '25': { 'label': 'Z', 'parent': ['A', 'Q'] }
+*/
 };
 
 export {
